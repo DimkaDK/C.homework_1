06dcabe1f25fd52802cb15c72d6d9624ff9e1095
DmitryiKD <dimka_2284@mail.ru>
2022-11-23 15:53:04 +0300

    Added task_6

---
+++ task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите число: ");
+
+int Number = int.Parse(Console.ReadLine());
+
+if (Number%2 == 0)
+{
+    Console.WriteLine ("Это четное число");
+}
+else
+{
+    Console.WriteLine ("Это нечетное число");
+}
