08f68a6c75cf66d2d24a5bce708462233f869f02
DmitryiKD <dimka_2284@mail.ru>
2022-11-23 15:40:40 +0300

    Added task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write ("Введите первое число: ");
+
+int Number1 = int.Parse(Console.ReadLine());
+
+Console.Write ("Введите второе число: ");
+
+int Number2 = int.Parse(Console.ReadLine());
+
+Console.Write ("Введите третье число: ");
+
+int Number3 = int.Parse(Console.ReadLine());
+
+int max = Number1;
+
+if (max < Number2)
+{
+    max = Number2;
+}
+if (max < Number3)
+{
+    max = Number3;
+}
+
+Console.WriteLine ($" Число {max} наибольшее из этих трех чисел");
