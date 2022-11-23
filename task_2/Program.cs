434f90ad444cbc7ec41faebb830854d88a8c4d38
DmitryiKD <dimka_2284@mail.ru>
2022-11-23 15:30:31 +0300

    Added task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write ("Введите первое число: ");
+
+int fistNumber = int.Parse(Console.ReadLine());
+
+Console.Write ("Введите второе число: ");
+
+int secondNumber = int.Parse(Console.ReadLine());
+
+if (fistNumber == secondNumber) 
+{
+    Console.WriteLine ($" {fistNumber} равно {secondNumber}");
+    return;
+}
+
+if (fistNumber > secondNumber)
+{
+    Console.WriteLine ($" {fistNumber} больше {secondNumber}");
+}
+else
+{
+    Console.WriteLine ($" {secondNumber} больше {fistNumber}");
+}
