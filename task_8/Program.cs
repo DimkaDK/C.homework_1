e05195e1b2052ac5be822dfb471ef71149acc944
DmitryiKD <dimka_2284@mail.ru>
2022-11-23 16:12:45 +0300

    Added task_8

---
+++ task_8/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write ("Введите число (число должно быть больше 1): ");
+
+int Number = int.Parse(Console.ReadLine());
+
+if (Number <= 1)
+{
+    Console.WriteLine ("Число задано неверно");
+    return;
+}
+
+int index = 1;
+
+while (index <= Number)
+{
+    if (index%2 == 0)
+    {
+        Console.Write ($"{index}, ");       
+    }
+     index++;
+}
